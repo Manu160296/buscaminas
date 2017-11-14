5eddebf8710edbb88af065e798f42ff3ea27c1be
Manu160296 <fmanuela499@gmail.com>
2017-11-14 03:48:04 -0500

    corrigiend estructura js

--- js/app.js
+++ js/app.js
@@ -1,71 +1,63 @@
-var tableBombIt = [
-                  ' ',
-                  '*',
-                  '1',
-                  ' ',
-                  '\n',
-                  ' ',
-                  ' ',
-                  '*',
-                  '2',
-                  '\n',
-                  '1',
-                  '*',
-                  ' ',
-                  ' ',
-                  '\n',
-                  ' ',
-                  '3',
-                  '*',
-                  ' ',
-                ];
+window.onload= function (){
+  var BOMB='*';
+      BLANK='';
+      SHOW='v';
+      bombs=2;
+var colors = {red:'red',  green: 'green', lightgray:'lightgray',  gray:'gray',  darkgray:'7a7575'};
+var text ={winner: 'Ganaste!!!', loser: 'Perdiste!!!'}
+var matrixOrigin= [ [1,1,1,BLANK],
+                    [1,BOMB,1,BLANK],
+                    [1,1,2,1],
+                    [BLANK,BLANK,1,BOMB]  ]
 
-var containerBombIt = document.getElementById('container-bomb-it');
-var bombIt = document.getElementById('bomb-it');
+var matrixView=  [ [BLANK,BLANK,BLANK],
+                   [BLANK,BLANK,BLANK],
+                   [BLANK,BLANK,BLANK],
+                   [BLANK,BLANK,BLANK] ]
 
- var drawGameZone = function (array) {
-  for(var i =0 ; i<array.length ; i++){
+var board = document.querySelector('.board-js');
+    info = document.querySelector('.info-js');
+    buttonStart= document.querySelector('.button-start-js');
+    buttonReset = document.querySelector('.button-reset-js');
+    buttonReset.addEventListener('click',reset);
+    buttonStart.addEventListener('click',start);
 
-    var cell = document.createElement('div');
-    var enter = document.createElement('br');
-
-    if(array[i]=== '1' || array[i]=== '2' || array[i] === '3'){
-      cell.className = 'number';
-      cell.innerText = array[i];
-      cell.onclick = showNumber;
-    } else  if (array[i] == ' '){
-      cell.className = 'emptySpace';
-      cell.onclick= changeColor;
-    } else if (array[i] == '*'){
-      cell.className = 'bomb';
-      cell.onclick= boom;
-    } else {
-      bombIt.appendChild(enter);
-      continue;
+    function start() {
+      reset();
+  board.addEventListener('click',displayCell);
+  board.addEventListener('mouseOver',cellMouseOver);
+  board.addEventListener('mouseout',cellMouseOut);
     }
-    bombIt.appendChild(cell);
-  }
-  addButtonRestart();
-}
 
-var showNumber = function () {
-  this.className ='number number-show'
-};
-var changeColor =function () {
-  this.style.background = 'black';
-};
-var boom = function () {
-  containerBombIt.removeChild(bombIt);
-  containerBombIt.innerHTML = '<img src="assets/images/giphy.gif" alt="Boom!">';
-  addButtonRestart();
+    function cellMouseOver(event) {
+      event.target.style.background=colors.darkgray;
+    }
+
+    function cellMouseOut(event) {
+      event.target.style.background=colors.gray;
+    }
+
+    function reset () {
+      info.textContent= BLANK;
+      var cells=document.querySelectorAll('td');
+
+      for (var i = 0; i < cells.length; i++) {
+        cells[i].textContent= BLANK;
+        cells[i].style.background=colors.gray;
+      }
+
+      for (var i = 0; i < matrixView.length; i++) {
+        for (var j = 0; j < matrixView.length; j++) {
+          matrixView[i][j]=BLANK;
+        }
+      }
+    }
+
+    function displayCell(event) {
+        if( event.target.matches('td') ){
+          var value = getMatrixValue(event);
+
+          if(value !==SHOW)
+        }
+    }
 }
-var addButtonRestart = function () {
-  var buttonRestart = document.createElement('button');
-  buttonRestart.innerText= 'Restart!!!';
-  buttonRestart.addEventListener('click',reStart);
-  containerBombIt.appendChild(buttonRestart);
-}
-var reStart = function () {
-  location.reload();
-}
-drawGameZone(tableBombIt);
